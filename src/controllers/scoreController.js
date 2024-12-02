fb23713e7ec2465624b4f6bd01d08710b7624aff
GabrielCose <gabriel.caraujo@sptech.school>
2024-12-02 13:34:21 -0300

    Listar

--- src/controllers/scoreController.js
+++ src/controllers/scoreController.js
@@ -1,64 +1,19 @@
 var scoreModel = require("../models/scoreModel");
 
-// function listar(req, res) {
-//     scoreModel.listar().then(function (resultado) {
-//         if (resultado.length > 0) {
-//             res.status(200).json(resultado);
-//         } else {
-//             res.status(204).send("Nenhum resultado encontrado!")
-//         }
-//     }).catch(function (erro) {
-//         console.log(erro);
-//         console.log("Houve um erro ao buscar os scores: ", erro.sqlMessage);
-//         res.status(500).json(erro.sqlMessage);
-//     });
-// }
 
-// function listarPorUsuario(req, res) {
-//     var idUsuario = req.params.idUsuario;
-
-//     scoreModel.listarPorUsuario(idUsuario)
-//         .then(
-//             function (resultado) {
-//                 if (resultado.length > 0) {
-//                     res.status(200).json(resultado);
-//                 } else {
-//                     res.status(204).send("Nenhum resultado encontrado!");
-//                 }
-//             }
-//         )
-//         .catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log(
-//                     "Houve um erro ao buscar os scores: ",
-//                     erro.sqlMessage
-//                 );
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         );
-// }
-
-// function pesquisarDescricao(req, res) {
-//     var descricao = req.params.descricao;
-
-//     scoreModel.pesquisarDescricao(descricao)
-//         .then(
-//             function (resultado) {
-//                 if (resultado.length > 0) {
-//                     res.status(200).json(resultado);
-//                 } else {
-//                     res.status(204).send("Nenhum resultado encontrado!");
-//                 }
-//             }
-//         ).catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log("Houve um erro ao buscar os scores: ", erro.sqlMessage);
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         );
-// }
+function listar(req, res) {
+    scoreModel.listar().then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar os scores: ", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
 
 function inserirPontuacao(req, res) {
     var idUsuario = req.params.idUsuario;
@@ -84,49 +39,9 @@ console.log(idUsuario + `Esse é o id do usuario`)
 
 }
 
-// function editar(req, res) {
-//     var novaDescricao = req.body.descricao;
-//     var idscore = req.params.idscore;
 
-//     scoreModel.editar(novaDescricao, idscore)
-//         .then(
-//             function (resultado) {
-//                 res.json(resultado);
-//             }
-//         )
-//         .catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log("Houve um erro ao realizar o post: ", erro.sqlMessage);
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         );
-
-// }
-
-// function deletar(req, res) {
-//     var idscore = req.params.idscore;
-
-//     scoreModel.deletar(idscore)
-//         .then(
-//             function (resultado) {
-//                 res.json(resultado);
-//             }
-//         )
-//         .catch(
-//             function (erro) {
-//                 console.log(erro);
-//                 console.log("Houve um erro ao deletar o post: ", erro.sqlMessage);
-//                 res.status(500).json(erro.sqlMessage);
-//             }
-//         );
-// }
 
 module.exports = {
-    // listar,
-    // listarPorUsuario,
-    // pesquisarDescricao,
+    listar,
     inserirPontuacao
-    // editar,
-    // deletar
 }
--- src/models/scoreModel.js
+++ src/models/scoreModel.js
@@ -1,47 +1,18 @@
     var database = require("../database/config");
 
-// function listar() {
-//     console.log("ACESSEI O Score  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
-//     var instrucaoSql = `
-//         SELECT s.idScore, s.pontos as Pontuação,u.nome as "Nome"
-//             FROM score AS s
-//             JOIN usuario AS u
-//             ON fkUsuario = idUsuario
-//             ORDER BY s.pontos desc;
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
-
-// function pesquisarDescricao(texto) {
-//     console.log("ACESSEI O Score MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function pesquisarDescricao()");
-//     var instrucaoSql = `
-//         SELECT 
-//             a.id AS idScore,
-//             a.titulo,
-//             a.descricao,
-//             a.fk_usuario,
-//             u.id AS idUsuario,
-//             u.nome,
-//             u.email,
-//             u.senha
-//         FROM Score a
-//             INNER JOIN usuario u
-//                 ON a.fk_usuario = u.id
-//         WHERE a.descricao LIKE '${texto}';
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
-
-// function listarPorUsuario(idUsuario) {
-//     console.log("ACESSEI O Score MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarPorUsuario()");
-//     var instrucaoSql = `
-//         SELECT pontos, fkUsuario FROM score WHERE fkUsuario = ${idUsuario};
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
+    function listar() {
+        // console.log("ACESSEI O Score  MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listar()");
+        var instrucaoSql = `
+                SELECT MAX(s.pontos) AS pontuacao, u.nome AS 'nome'
+                FROM score AS s
+                JOIN usuario as u
+                ON s.fkUsuario = u.idUsuario
+                GROUP BY u.idUsuario
+                ORDER BY pontuacao DESC;
+        `;
+        console.log("Executando a instrução SQL: \n" + instrucaoSql);
+        return database.executar(instrucaoSql);
+    }
 
 function inserirPontuacao (pontos,idUsuario) {
     console.log("ACESSEI O Score MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function inserirPontuacao  (): ",idUsuario);
@@ -52,29 +23,9 @@ function inserirPontuacao (pontos,idUsuario) {
     return database.executar(instrucaoSql);
 }
 
-// function editar(pontos,idUsuario) {
-//     console.log("ACESSEI O Score MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function editar(): ", pontos, idUsuario);
-//     var instrucaoSql = `
-//         UPDATE score SET pontos = ${pontos} WHERE fkUsuario = ${idUsuario};
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
 
-// function deletar(idScore) {
-//     console.log("ACESSEI O Score MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function deletar():", idScore);
-//     var instrucaoSql = `
-//         DELETE FROM Score WHERE id = ${idScore};
-//     `;
-//     console.log("Executando a instrução SQL: \n" + instrucaoSql);
-//     return database.executar(instrucaoSql);
-// }
 
 module.exports = {
-    // listar,
-    // listarPorUsuario,
-    // pesquisarDescricao,
+    listar,
     inserirPontuacao
-    // editar,
-    // deletar
 }
